630a731d4fa21628dea65daa3c35c6f59247d85b
hjalle <djacob@kth.se>
2020-06-29 18:48:44 +0200

    increased readability, made code more modular and easily to adjust formatting. It's better now

--- dancing_penguin.c
+++ dancing_penguin.c
@@ -1,36 +1,40 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+
+const char left_facing[]  = "< 8      ";
+const char right_facing[] = "      8 >";
+const char up_arms[]      = "\\ ( U ) /";
+const char down_arms[]    = "/ ( U ) \\";
+const char step_left[]    = "_     _  ";
+const char step_right[]   = "  _     _";
+const char spaces[]       = "         ";
+const char box_lid[]      = "+-------------------+";
+
+const int offset = 13;
 
 int main(int argc, char const *argv[]) {
-  char left_facing[]  = "< 8      ";
-  char right_facing[] = "      8 >";
-  char up_arms[]      = "\\ ( U ) /";
-  char down_arms[]    = "/ ( U ) \\";
-  char step_left[]    = "_     _  ";
-  char step_right[]   = "  _     _";
-  char spaces[]       = "         ";
-  char box_lid[]      = "---------------";
+
+  int box_width = strlen(box_lid);
+  int margin = (box_width - strlen(up_arms)) / 2;
 
   int i = 1;
   while (i < 3) {
+    printf("%*s%s\n", offset, "", box_lid);
+    printf("%*s%-*s|\n", offset, "", box_width - 1, "|");
+    printf("%*s%-*s|\n", offset, "", box_width - 1, "|");
     if (i % 2 == 0) {
-      printf("%13s%s%s%s\n", "", "+", box_lid, "+");
-      printf("%13s%-4s%s%4s\n", "", "|", spaces, "|");
-      printf("%13s%-4s%s%4s\n", "", "|", left_facing, "|");
-      printf("%13s%-4s%s%4s\n", "", "|", up_arms, "|");
-      printf("%13s%-4s%s%4s\n", "", "|", step_left, "|");
-      printf("%13s%-4s%s%4s\n", "", "|", spaces, "|");
-      printf("%13s%s%s%s\n", "", "+", box_lid, "+");
+      printf("%*s%-*s%s%*s\n", offset, "", margin, "|", left_facing, margin, "|");
+      printf("%*s%-*s%s%*s\n", offset, "", margin, "|", up_arms, margin, "|");
+      printf("%*s%-*s%s%*s\n", offset, "", margin, "|", step_left, margin, "|");
     } else {
-      printf("%13s%s%s%s\n", "", "+", box_lid, "+");
-      printf("%13s%-4s%s%4s\n", "", "|", spaces, "|");
-      printf("%13s%-4s%s%4s\n", "", "|", right_facing, "|");
-      printf("%13s%-4s%s%4s\n", "", "|", down_arms, "|");
-      printf("%13s%-4s%s%4s\n", "", "|", step_right, "|");
-      printf("%13s%-4s%s%4s\n", "", "|", spaces, "|");
-      printf("%13s%s%s%s\n", "", "+", box_lid, "+");
+      printf("%*s%-*s%s%*s\n", offset, "", margin, "|", right_facing, margin, "|");
+      printf("%*s%-*s%s%*s\n", offset, "", margin, "|", down_arms, margin, "|");
+      printf("%*s%-*s%s%*s\n", offset, "", margin, "|", step_right, margin, "|");
     }
-    printf("%s\n", "");
+    printf("%*s%-*s|\n", offset, "", box_width - 1, "|");
+    printf("%*s%-*s|\n", offset, "", box_width - 1, "|");
+    printf("%*s%s\n", offset, "", box_lid);
     sleep(1);
     i++;
   }
